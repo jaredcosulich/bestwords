ec219c4bcd66cb1016516b47b3522274bbac9679
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-04-04 12:32:25 -0700

    - reporting

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,10 @@
 class Word < ActiveRecord::Base
+  after_create :assign_user_words
+
+  def assign_user_words
+    UserWord.where("word_id is null").where("custom_word = ?", word).each do |user_word|
+      user_word.update_attributes(:word_id => id)
+    end
+  end
 
 end
