67619ab570a5764c76ee2c51a78992077c4eb324
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-04-04 17:06:37 -0700

    - your_name on front page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def create
+    if params[:user][:slug].blank? || params[:user][:slug] == "your_name"
+      redirect_to root_path
+      return
+    end
     @user = User.create(params[:user].merge(:email => "tmp#{Time.new.to_i}@example.com", :password => User::FAKE_PASSWORD, :password_confirmation => User::FAKE_PASSWORD))
     sign_in(@user)
     redirect_to profile_path(@user)
