3228a972c0eedcc13af207c317545ffd247c5908
Jared Cosulich <jared@irrationaldesign.com>
2014-09-24 22:07:30 -0400

    removing sendgrid credentials

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -3,6 +3,6 @@ OmniauthDeviseExample::Application.config.action_mailer.smtp_settings= {
   :port => '25',
   :domain => Rails.application.host,
   :authentication => :plain,
-  :user_name => "services@irrationaldesign.com",
-  :password => "sam7panda"
+  :user_name => ENV['SENDGRID_EMAIL'],
+  :password => ENV['SENDGRID_PASSWORD']
 }
